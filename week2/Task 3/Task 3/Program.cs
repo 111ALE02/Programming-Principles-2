b0f50f1e893414c29a2ba91ff50b2c4ea65cd038
ALEN <sad.alan@bk.ru>
2019-02-15 23:06:26 +0600

    Lab 2

---
+++ week2/Task 3/Task 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void print(FileSystemInfo fi, int z)
+        {
+            string s = new string(' ', z);
+            s = s + fi.Name;
+            Console.WriteLine(s);
+
+            if (fi.GetType() == typeof(DirectoryInfo))
+            {
+                var v = (fi as DirectoryInfo).GetFileSystemInfos();
+                foreach (var n in v)
+                {
+                    print(n, z + 5);
+                }
+            }
+
+        }
+        static void Main()
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\firstadik\Desktop\PP2");
+            print(di, 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2/Task 4/Task 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\Users\firstadik\Desktop\PP2\week2\Task 4\Mc.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sr = new StreamWriter(fs);
+            sr.WriteLine("Alen");
+            sr.Close();
+            fs.Close();
+            File.Copy(@"C:\Users\firstadik\Desktop\PP2\week2\Task 4\Mc.txt", @"C:\Users\firstadik\Desktop\PP2\week2\Task 4\Alen\Mc.txt");
+
+            File.Delete(@"C:\Users\firstadik\Desktop\PP2\week2\Task 4\Mc.txt");
+
+
+
+        }
+    }
+}
